9fe80795bc19d7446dcc83d4752365cf01643f6b
KimDoubleB <tree9295@gmail.com>
2021-02-17 00:28:23 +0900

    feat: Create post list response dto class

---
+++ springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.tistory.bbdevstory.springboot.web.dto;
+
+import com.tistory.bbdevstory.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
