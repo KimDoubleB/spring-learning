f8a6cee8da32979da324ee90ce0ccd6d346264a6
KimDoubleB <tree9295@gmail.com>
2021-02-16 23:50:20 +0900

    feat: add index page using controller, mustache

---
+++ springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.tistory.bbdevstory.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
