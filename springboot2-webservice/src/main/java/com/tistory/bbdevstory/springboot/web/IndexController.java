126186d601c1cedf4591d4b2f44769cdf327ea38
KimDoubleB <tree9295@gmail.com>
2021-02-17 00:30:32 +0900

    feat: update index page - deliver post list to index view

--- springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/IndexController.java
+++ springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.tistory.bbdevstory.springboot.web;
 
+import com.tistory.bbdevstory.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
     @GetMapping("/posts/save")
