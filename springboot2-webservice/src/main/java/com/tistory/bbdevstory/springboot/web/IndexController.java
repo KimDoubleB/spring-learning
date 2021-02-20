f1b12ee1251923b0eca632e3ce580a90d2e08312
KimDoubleB <tree9295@gmail.com>
2021-02-20 12:22:30 +0900

    feat: add update posts function

--- springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/IndexController.java
+++ springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.tistory.bbdevstory.springboot.web;
 
 import com.tistory.bbdevstory.springboot.service.posts.PostsService;
+import com.tistory.bbdevstory.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,8 +19,17 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
--- springboot2-webservice/src/main/resources/static/js/app/index.js
+++ springboot2-webservice/src/main/resources/static/js/app/index.js
@@ -32,7 +32,28 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
-    }
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
 };
 
 main.init();
