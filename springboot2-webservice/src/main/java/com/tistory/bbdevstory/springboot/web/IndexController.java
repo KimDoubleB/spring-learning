ab85f8afacccf26821bc3ee693eb406a02d19508
KimDoubleB <tree9295@gmail.com>
2021-02-28 02:14:51 +0900

    feat: LoginUser 어노테이션 적용

--- springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/IndexController.java
+++ springboot2-webservice/src/main/java/com/tistory/bbdevstory/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.tistory.bbdevstory.springboot.web;
 
+import com.tistory.bbdevstory.springboot.config.auth.LoginUser;
 import com.tistory.bbdevstory.springboot.config.auth.dto.SessionUser;
 import com.tistory.bbdevstory.springboot.service.posts.PostsService;
 import com.tistory.bbdevstory.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,8 @@ public class IndexController {
 
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
