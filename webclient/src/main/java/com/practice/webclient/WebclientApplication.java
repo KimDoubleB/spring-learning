ff4f50ab4c73e340b3031ecac387fa06087c6672
KimDoubleB <tree9295@gmail.com>
2021-03-30 23:54:13 +0900

    init: WebClient practice project

---
+++ webclient/src/main/java/com/practice/webclient/WebclientApplication.java
@@ -0,0 +1,13 @@
+package com.practice.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebclientApplication.class, args);
+    }
+
+}
