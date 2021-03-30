123538ac99ae19fdd598a321878e43a4768e0958
KimDoubleB <tree9295@gmail.com>
2021-03-31 00:34:30 +0900

    feat: add Response about JsonPlace site

---
+++ webclient/src/main/java/com/practice/webclient/dto/JsonPlaceResponse.java
@@ -0,0 +1,4 @@
+package com.practice.webclient.dto;
+
+public interface JsonPlaceResponse {
+}
---
+++ webclient/src/main/java/com/practice/webclient/dto/UserPostResponse.java
@@ -0,0 +1,17 @@
+package com.practice.webclient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UserPostResponse implements JsonPlaceResponse {
+    private Long userId;
+    private Long postId;
+    private String title;
+    private String body;
+}
