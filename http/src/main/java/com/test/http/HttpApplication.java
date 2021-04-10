a9ce3747f849272d91d46c842609730d91c0059e
KimDoubleB <tree9295@gmail.com>
2021-04-10 17:40:41 +0900

    init: http test project

---
+++ http/src/main/java/com/test/http/HttpApplication.java
@@ -0,0 +1,14 @@
+package com.test.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpApplication {
+
+    // Webflux
+    public static void main(String[] args) {
+        SpringApplication.run(HttpApplication.class, args);
+    }
+
+}
