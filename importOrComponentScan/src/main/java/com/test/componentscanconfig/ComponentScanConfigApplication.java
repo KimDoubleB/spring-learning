56f3b033da92f5e53866dd1e0c09be680f7811cf
KimDoubleB <tree9295@gmail.com>
2021-04-18 00:24:05 +0900

    feat: maven pom.xml example

--- importOrComponentScan/src/main/java/com/test/componentscanconfig/ComponentScanConfigApplication.java
+++ importOrComponentScan/src/main/java/com/test/componentscanconfig/ComponentScanConfigApplication.java
@@ -2,9 +2,10 @@ package com.test.componentscanconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
 public class ComponentScanConfigApplication {
     private static ApplicationContext ac;
 
