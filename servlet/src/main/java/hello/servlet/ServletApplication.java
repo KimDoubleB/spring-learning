8a7d0ac89cee6add0ef66e33c726034dde33aba7
KimDoubleB <tree9295@gmail.com>
2021-07-21 00:12:06 +0900

    feat: init servlet project

---
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
---
+++ servlet/src/test/java/hello/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
