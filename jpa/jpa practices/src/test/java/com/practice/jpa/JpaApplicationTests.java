70babe8537b46d1180fa3e71bcee92b7927a858c
KimDoubleB <tree9295@gmail.com>
2021-10-03 16:01:59 +0900

    feat: added jpa practices project

---
+++ jpa/jpa practices/src/test/java/com/practice/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
