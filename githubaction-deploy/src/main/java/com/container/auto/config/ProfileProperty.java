d24b871e5847c7be48d193aa916e6e30a2bce951
Bobae Kim <tree9295@gmail.com>
2022-02-14 02:09:41 +0900

    Revert "Feature/GitHub action"

--- githubaction-deploy/src/main/java/com/container/auto/config/ProfileProperty.java
+++
@@ -1,7 +0,0 @@
-package com.container.auto.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "server")
-public record ProfileProperty(String profile) {
-}
