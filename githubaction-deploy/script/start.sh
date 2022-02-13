a5f5a82ccbb390d925c1e56aea03572e98ad62c2
KimDoubleB <tree9295@gmail.com>
2022-02-14 01:09:40 +0900

    split local/prod port

--- githubaction-deploy/script/start.sh
+++ githubaction-deploy/script/start.sh
@@ -10,9 +10,16 @@ then
     exit 0
 fi
 
-envFileName=`echo "$containerTag" | awk -F- '{ print $1 }'`
-containerId=`docker ps | grep "$containerImage" | awk '{ print $1 }'`
-env="/home/ec2-user/github-action/$envFileName.env"
+profile=`echo "$containerTag" | awk -F- '{ print $1 }'`
+containerId=`docker ps | grep "$containerImage:$profile" | awk '{ print $1 }'`
+env="/home/ec2-user/github-action/$profile.env"
+
+# set port
+port=8080
+if [ $profile = "local" ]
+then
+    port=8081
+fi
 
 # stop container
 if [ ! -z "$containerId" ]
@@ -21,4 +28,4 @@ then
 fi
 
 # start container
-docker run -d -p 8080:8080 --env-file "$env" "$containerImage":"$containerTag"
+docker run -d -p $port:$port --env-file $env $containerImage:$containerTag
