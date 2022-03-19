1603da678c559423ae192b3b2e09ae24c2ab815c
KimDoubleB <tree9295@gmail.com>
2022-03-19 18:15:27 +0900

    feat: intial commit (queue-sqs)

---
+++ queue-sqs/src/main/java/com/queue/push/PushApplication.java
@@ -0,0 +1,13 @@
+package com.queue.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushApplication.class, args);
+	}
+
+}
---
+++ queue-sqs/src/test/java/com/queue/push/PushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queue.push;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
