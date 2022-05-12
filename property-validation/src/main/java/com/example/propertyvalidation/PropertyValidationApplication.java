86789d17be2b110763d1b50a1ad53e7494c30607
KimDoubleB <tree9295@gmail.com>
2022-05-13 00:07:01 +0900

    Init project: property validation

---
+++ property-validation/src/main/java/com/example/propertyvalidation/PropertyValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.propertyvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyValidationApplication.class, args);
+	}
+
+}
---
+++ property-validation/src/test/java/com/example/propertyvalidation/PropertyValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propertyvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
