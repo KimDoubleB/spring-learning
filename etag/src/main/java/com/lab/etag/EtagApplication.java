0d09f02f16d24d32c0b2608ccf199a1854b93735
KimDoubleB <tree9295@gmail.com>
2023-03-15 01:11:53 +0900

    feat: etag init

---
+++ etag/src/main/java/com/lab/etag/EtagApplication.java
@@ -0,0 +1,13 @@
+package com.lab.etag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtagApplication.class, args);
+	}
+
+}
