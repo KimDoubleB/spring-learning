7c6ee54232ccfd3c3a5da7dc3f1b6a9d1e695943
KimDoubleB <tree9295@gmail.com>
2023-03-21 03:25:29 +0900

    feat: reproject serverless-native

---
+++ serverless/src/main/java/com/kimdoubleb/serverless/ServerlessApplication.java
@@ -0,0 +1,13 @@
+package com.kimdoubleb.serverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerlessApplication.class, args);
+	}
+
+}
