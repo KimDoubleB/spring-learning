c05ceed518433a071c836872b0e24cff82474886
KimDoubleB <tree9295@gmail.com>
2023-04-04 19:21:50 +0900

    feat: init api-versioning project

---
+++ api-versioning/src/test/java/com/version/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
