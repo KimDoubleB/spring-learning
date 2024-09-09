261e4cde3cf2dc8aefcf20a706ed0ae077e20eda
KimDoubleB <tree9295@gmail.com>
2024-09-10 00:26:14 +0900

    feat: set git info plugin

--- actuator/build.gradle.kts
+++ actuator/build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.3.3"
-	id("io.spring.dependency-management") version "1.1.6"
     application
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
 
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
+    id("com.gorylenko.gradle-git-properties") version "2.4.1"
 }
 
 group = "com.binux"
